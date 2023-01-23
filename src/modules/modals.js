6d706ad01ceac9f20f92aa4a69405e122feefc87
gotodoweb <gotodo@internet.ru>
2023-01-23 21:15:19 +0500

    feat:substrate for the pop-up window

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,7 +1,7 @@
 export const openModal = (modal) => {
 
   // создаем блок 
-  const layout = document.createElement(div);
+  const layout = document.createElement('div');
   layout.classList.add('modal-backdrop');
   layout.classList.add('fade');
   document.body.append(layout);
@@ -10,15 +10,21 @@ export const openModal = (modal) => {
   modal.classList.add("d-block");
 
   setTimeout(() => {
+    layout.classList.add("show");
     modal.classList.add("show");
   }, 200);
 };
 
 export const closeModal = (modal) => {
+  const layout = document.querySelector('.modal-backdrop');
+  layout.classList.remove("show");
+
   modal.classList.remove("show");
 
   setTimeout(() => {
     modal.classList.remove("d-block");
+    layout.remove();
+
   }, 400);
 };
 
